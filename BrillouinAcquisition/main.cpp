9d38e157ab49994db49abb87b794139799e8e30d
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2017-09-29 14:17:15 +0200

    Enable High-DPI scaling

--- BrillouinAcquisition/main.cpp
+++ BrillouinAcquisition/main.cpp
@@ -15,6 +15,17 @@ QScopedPointer<QFile> m_logFile;
 void loggingHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg);
 
 int main(int argc, char *argv[]) {
+
+	#ifdef Q_OS_WIN
+		SetProcessDPIAware(); // call before the main event loop
+	#endif // Q_OS_WIN
+
+	#if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
+		QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
+	#else
+		qputenv("QT_DEVICE_PIXEL_RATIO", QByteArray("1"));
+	#endif // QT_VERSION
+
 	QApplication a(argc, argv);
 	BrillouinAcquisition w;
 
