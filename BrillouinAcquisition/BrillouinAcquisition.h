dfafc70d12209b565d45f2697b53bb6505559df5
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2017-11-16 17:40:04 +0100

    Add default camera settings

--- BrillouinAcquisition/BrillouinAcquisition.h
+++ BrillouinAcquisition/BrillouinAcquisition.h
@@ -8,6 +8,33 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_BrillouinAcquisition.h"
 
+typedef struct {
+	AT_64 left   = 1;
+	AT_64 width  = 2048;
+	AT_64 bottom = 1;
+	AT_64 height = 2048;
+	AT_WC *binning = L"1x1";
+} CAMERA_ROI;
+
+typedef struct {
+	AT_WC *rate			= L"100 MHz";
+	AT_WC *sensitivity	= L"16-bit (low noise & high well capacity)";
+	AT_WC *encoding		= L"Mono16";
+	AT_WC *mode			= L"Fixed";
+} CAMERA_READOUT;
+
+typedef struct {
+	double exp = 0.5;					// [s]	exposure time
+	int nrImages = 2;					// [1]	number of images to acquire at one point
+	AT_WC *triggerMode = L"Software";	//		trigger mode
+	CAMERA_ROI roi;						//		region of interest
+	CAMERA_READOUT readout;				//		readout settings
+} SETTINGS_CAMERA;
+
+typedef struct {
+
+} SETTINGS_STAGE;
+
 class BrillouinAcquisition : public QMainWindow {
 	Q_OBJECT
 
