16132ec639a58d3485cf86d69e1988de64fa41d5
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-02-21 11:34:02 +0100

    Add class for controlling microscope and stage

---
+++ BrillouinAcquisition/scancontrol.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "scancontrol.h"
+
+ScanControl::ScanControl() {
+}
+
+ScanControl::~ScanControl() {
+}
---
+++ BrillouinAcquisition/scancontrol.h
@@ -0,0 +1,10 @@
+#ifndef SCANCONTROL_H
+#define SCANCONTROL_H
+
+class ScanControl {
+public:
+	ScanControl();
+	~ScanControl();
+};
+
+#endif // SCANCONTROL_H
