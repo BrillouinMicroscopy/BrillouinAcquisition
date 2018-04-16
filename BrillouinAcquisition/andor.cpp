fb2b60987424e8f2269e8f623aadb57c10d4e9f7
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-04-16 18:24:01 +0200

    Always use full ROI as preview size

--- BrillouinAcquisition/andor.cpp
+++ BrillouinAcquisition/andor.cpp
@@ -222,6 +222,16 @@ void Andor::prepareAcquisition() {
 	int pixelNumber = m_settings.roi.width * m_settings.roi.height;
 	liveBuffer = new CircularBuffer<AT_U8>(5, pixelNumber * 2);
 
+	AT_SetInt(m_cameraHndl, L"AOIWidth", m_options.ROIWidthLimits[1]);
+	AT_SetInt(m_cameraHndl, L"AOILeft", m_options.ROIWidthLimits[0]);
+	AT_SetInt(m_cameraHndl, L"AOIHeight", m_options.ROIHeightLimits[1]);
+	AT_SetInt(m_cameraHndl, L"AOITop", m_options.ROIHeightLimits[0]);
+
+	AT_GetInt(m_cameraHndl, L"AOIHeight", &m_settings.roi.height);
+	AT_GetInt(m_cameraHndl, L"AOIWidth", &m_settings.roi.width);
+	AT_GetInt(m_cameraHndl, L"AOILeft", &m_settings.roi.left);
+	AT_GetInt(m_cameraHndl, L"AOITop", &m_settings.roi.top);
+
 	// Start acquisition
 	AT_Command(m_cameraHndl, L"AcquisitionStart");
 	AT_InitialiseUtilityLibrary();
