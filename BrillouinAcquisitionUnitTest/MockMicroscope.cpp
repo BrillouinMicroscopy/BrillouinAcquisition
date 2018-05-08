3dfcfced08689c3e0758ee12b19e8b58f2df18ce
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-05-08 11:27:36 +0200

    Remove commented code

--- BrillouinAcquisitionUnitTest/MockMicroscope.cpp
+++ BrillouinAcquisitionUnitTest/MockMicroscope.cpp
@@ -27,35 +27,3 @@ qint64 MockMicroscope::writeToDevice(const char *data) {
 std::string MockMicroscope::readOutputBuffer() {
 	return m_outputBuffer;
 }
-
-/*qint64 MockMicroscope::readCharacter(char *data, qint64 maxlen) {
-	if (m_outputBuffer.size() > 0) {
-		char tmp = m_outputBuffer[0];
-		strcpy(data, &tmp);
-		m_outputBuffer.remove(0, 1);
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-qint64 MockMicroscope::writeData(const char *data, qint64 maxSize) {
-	std::string request = data;
-	std::string answer;
-	if (request == "FPZp\r")
-		answer = "PF000000\r";
-	if (request == "NPXp\r")
-		answer = "PN000000\r";
-	if (request == "NPYp\r")
-		answer = "PN000000\r";
-	m_outputBuffer = answer.c_str();
-	return 1;
-}
-
-bool MockMicroscope::waitForBytesWritten(int msecs) {
-	return true;
-}
-
-bool MockMicroscope::waitForReadyRead(int msecs) {
-	return true;
-}*/
--- BrillouinAcquisitionUnitTest/MockMicroscope.h
+++ BrillouinAcquisitionUnitTest/MockMicroscope.h
@@ -15,16 +15,6 @@ public:
 
 	qint64 writeToDevice(const char *data);
 protected:
-	//qint64 readData(char* data, qint64 maxlen) Q_DECL_OVERRIDE;
-	//qint64 writeData(const char* data, qint64 len) Q_DECL_OVERRIDE;
-
 	bool open(OpenMode mode) Q_DECL_OVERRIDE;
 	void close() Q_DECL_OVERRIDE;
-
-	//qint64 send() Q_DECL_OVERRIDE;
-	//qint64 writeData(const char *data, qint64 maxSize);
-
-	//bool waitForBytesWritten(int msecs = 30000);
-
-	//bool waitForReadyRead(int msecs = 30000);
 };
