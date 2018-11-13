81f6ba17d566ce3a5eed3f418dc35cdfe9e4294e
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-11-13 15:26:39 +0100

    Move some NIDAQ functions to end of file

--- BrillouinAcquisition/src/Devices/NIDAQ.cpp
+++ BrillouinAcquisition/src/Devices/NIDAQ.cpp
@@ -2,76 +2,6 @@
 #include "NIDAQ.h"
 #include <windows.h>
 
-
-VOLTAGE2 NIDAQ::positionToVoltage(POINT2 position) {
-
-	position.x /= simplemath::sgn(m_calibration.fliplr);
-	position.y /= simplemath::sgn(m_calibration.flipud);
-
-	position = position - m_calibration.translation;
-
-	double x_rot = position.x * cos(m_calibration.rho) + position.y * sin(m_calibration.rho);
-	double y_rot = position.y * cos(m_calibration.rho) - position.x * sin(m_calibration.rho);
-
-	double R_old = sqrt(pow(position.x, 2) + pow(position.y, 2));
-
-	VOLTAGE2 voltage{ 0, 0 };
-	if (R_old == 0) {
-		voltage = { x_rot, y_rot };
-		return voltage;
-	}
-
-	// solve for R_new
-	
-	COEFFICIENTS5 coef = m_calibration.coef;
-	coef.e = -1 * R_old;
-	std::vector<std::complex<double>> solutions = simplemath::solveQuartic(coef);
-
-	// select real, positive solutions
-	std::vector<double> selected;
-	for (gsl::index i = 0; i < solutions.size(); i++) {
-		if (abs((solutions[i]).imag()) < 1e-10 && solutions[i].real() > 0) {
-			selected.push_back(solutions[i].real());
-		}
-	};
-
-	double R_new{ 0 };
-	if (selected.empty()) {
-		R_new = NAN;
-	} else if (selected.size() == 1) {
-		R_new = selected[0];
-	} else {
-		R_new = simplemath::min(selected);
-	}
-
-	voltage = { x_rot / R_old * R_new, y_rot / R_old * R_new };
-	return voltage;
-}
-
-POINT2 NIDAQ::voltageToPosition(VOLTAGE2 voltage) {
-
-	double R_old = sqrt(pow(voltage.Ux, 2) + pow(voltage.Uy, 2));
-	double R_new = m_calibration.coef.d * R_old + m_calibration.coef.c * pow(R_old, 2) +
-				   m_calibration.coef.b * pow(R_old, 3) + m_calibration.coef.a * pow(R_old, 4);
-
-	double Ux_rot = voltage.Ux * cos(m_calibration.rho) - voltage.Uy * sin(m_calibration.rho);
-	double Uy_rot = voltage.Ux * sin(m_calibration.rho) + voltage.Uy * cos(m_calibration.rho);
-
-	POINT2 position{ 0, 0 };
-	if (R_old == 0) {
-		position = { Ux_rot, Uy_rot };
-	} else {
-		position = { Ux_rot / R_old * R_new, Uy_rot / R_old * R_new };
-	}
-	
-	position = position + m_calibration.translation;
-
-	position.x *= simplemath::sgn(m_calibration.fliplr);
-	position.y *= simplemath::sgn(m_calibration.flipud);
-
-	return position;
-}
-
 NIDAQ::NIDAQ() noexcept {
 
 	m_deviceElements = {
@@ -544,4 +474,76 @@ void NIDAQ::centerPosition() {
 	Thorlabs_TIM::TIM_Home(m_serialNo_TIM, m_channelPosZ);
 	// set the scan position
 	applyScanPosition();
+}
+
+VOLTAGE2 NIDAQ::positionToVoltage(POINT2 position) {
+
+	position.x /= simplemath::sgn(m_calibration.fliplr);
+	position.y /= simplemath::sgn(m_calibration.flipud);
+
+	position = position - m_calibration.translation;
+
+	double x_rot = position.x * cos(m_calibration.rho) + position.y * sin(m_calibration.rho);
+	double y_rot = position.y * cos(m_calibration.rho) - position.x * sin(m_calibration.rho);
+
+	double R_old = sqrt(pow(position.x, 2) + pow(position.y, 2));
+
+	VOLTAGE2 voltage{ 0, 0 };
+	if (R_old == 0) {
+		voltage = { x_rot, y_rot };
+		return voltage;
+	}
+
+	// solve for R_new
+
+	COEFFICIENTS5 coef = m_calibration.coef;
+	coef.e = -1 * R_old;
+	std::vector<std::complex<double>> solutions = simplemath::solveQuartic(coef);
+
+	// select real, positive solutions
+	std::vector<double> selected;
+	for (gsl::index i = 0; i < solutions.size(); i++) {
+		if (abs((solutions[i]).imag()) < 1e-10 && solutions[i].real() > 0) {
+			selected.push_back(solutions[i].real());
+		}
+	};
+
+	double R_new{ 0 };
+	if (selected.empty()) {
+		R_new = NAN;
+	}
+	else if (selected.size() == 1) {
+		R_new = selected[0];
+	}
+	else {
+		R_new = simplemath::min(selected);
+	}
+
+	voltage = { x_rot / R_old * R_new, y_rot / R_old * R_new };
+	return voltage;
+}
+
+POINT2 NIDAQ::voltageToPosition(VOLTAGE2 voltage) {
+
+	double R_old = sqrt(pow(voltage.Ux, 2) + pow(voltage.Uy, 2));
+	double R_new = m_calibration.coef.d * R_old + m_calibration.coef.c * pow(R_old, 2) +
+		m_calibration.coef.b * pow(R_old, 3) + m_calibration.coef.a * pow(R_old, 4);
+
+	double Ux_rot = voltage.Ux * cos(m_calibration.rho) - voltage.Uy * sin(m_calibration.rho);
+	double Uy_rot = voltage.Ux * sin(m_calibration.rho) + voltage.Uy * cos(m_calibration.rho);
+
+	POINT2 position{ 0, 0 };
+	if (R_old == 0) {
+		position = { Ux_rot, Uy_rot };
+	}
+	else {
+		position = { Ux_rot / R_old * R_new, Uy_rot / R_old * R_new };
+	}
+
+	position = position + m_calibration.translation;
+
+	position.x *= simplemath::sgn(m_calibration.fliplr);
+	position.y *= simplemath::sgn(m_calibration.flipud);
+
+	return position;
 }
