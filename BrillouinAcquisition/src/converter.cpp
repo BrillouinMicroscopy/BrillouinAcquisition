2f7939b2c4e273445d4db9c17208de95bef2e5dd
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2019-07-26 20:42:47 +0200

    Delete commented code

--- BrillouinAcquisition/src/converter.cpp
+++ BrillouinAcquisition/src/converter.cpp
@@ -24,18 +24,6 @@ template <typename T>
 void converter::conv(PreviewBuffer<unsigned char>* previewBuffer, PLOT_SETTINGS* plotSettings, T* unpackedBuffer) {
 	int dim_x = previewBuffer->m_bufferSettings.roi.width;
 	int dim_y = previewBuffer->m_bufferSettings.roi.height;
-	//std::vector<double> x = simplemath::linspace(-30.0, 30.0, dim_x);
-	//std::vector<double> y = simplemath::linspace(-30.0, 30.0, dim_y);
-
-	//std::vector<double> inten;
-	//inten.resize(dim_x*dim_y);
-	//for (int ii{ 0 }; ii < dim_x; ii++) {
-	//	for (int jj{ 0 }; jj < dim_y; jj++) {
-	//		//double r = sqrt(pow(x[ii], 2) + pow(y[jj], 2));
-	//		//inten[ii + jj * dim_x] = sin(10 * r) / (10 * r);
-	//		inten[ii + jj * dim_x] = unpackedBuffer[ii + jj *dim_x];
-	//	}
-	//}
 
 	std::vector<double> converted(unpackedBuffer, unpackedBuffer + dim_x*dim_y);
 	switch (plotSettings->mode) {
