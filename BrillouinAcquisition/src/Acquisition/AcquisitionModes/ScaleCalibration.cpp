e057a3411846bd344b56b12006610a59a4be28e2
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2021-01-18 13:34:47 +0100

    Remove unused variables

--- BrillouinAcquisition/src/Acquisition/AcquisitionModes/ScaleCalibration.cpp
+++ BrillouinAcquisition/src/Acquisition/AcquisitionModes/ScaleCalibration.cpp
@@ -266,8 +266,6 @@ void ScaleCalibration::acquire() {
 	/*
 	 * We acquire three images here, one at the origin, and one each shifted in x- and y-direction.
 	 */
-	auto dx{ 10.0 };					// [µm]	shift in x-direction
-	auto dy{ 10.0 };					// [µm]	shift in y-direction
 	auto hysteresisCompensation{ 10.0 };// [µm] distance for compensation of the stage hysteresis
 	// Construct the positions
 	auto positions = std::vector<POINT2>{ { 0, 0 }, { m_Ds.x, 0 }, { 0, m_Ds.y } };
