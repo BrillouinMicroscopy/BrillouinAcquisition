4336efb0452902d91e645310f12727aad00a30ad
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2023-05-23 14:03:46 +0200

    Fix running unit tests

--- BrillouinAcquisitionUnitTest/NIDAQ_PositionVoltage.cpp
+++ BrillouinAcquisitionUnitTest/NIDAQ_PositionVoltage.cpp
@@ -15,7 +15,7 @@ namespace BrillouinAcquisitionUnitTest
 			NIDAQ *nidaq = new NIDAQ();
 			Acquisition *acquisition = new Acquisition(nullptr);
 			Camera *brightfieldCamera = nullptr;
-			VoltageCalibration *calibration = new VoltageCalibration(nullptr, acquisition, &brightfieldCamera, (ODTControl**)&nidaq);
+			VoltageCalibration *calibration = new VoltageCalibration(nullptr, acquisition, brightfieldCamera, (ODTControl*&)nidaq);
 			calibration->load("");
 			VOLTAGE2 voltage{ 0, 0 };
 			POINT2 answer = nidaq->voltageToPosition(voltage);
@@ -28,7 +28,7 @@ namespace BrillouinAcquisitionUnitTest
 			NIDAQ *nidaq = new NIDAQ();
 			Acquisition *acquisition = new Acquisition(nullptr);
 			Camera *brightfieldCamera = nullptr;
-			VoltageCalibration*calibration = new VoltageCalibration(nullptr, acquisition, &brightfieldCamera, (ODTControl**)&nidaq);
+			VoltageCalibration*calibration = new VoltageCalibration(nullptr, acquisition, brightfieldCamera, (ODTControl*&)nidaq);
 			calibration->load("");
 			VOLTAGE2 voltage{ 0, 0 };
 			POINT2 position = nidaq->voltageToPosition(voltage);
